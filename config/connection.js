3640ad21afeac78ce884e0d467d587a269e7f904
NekoCarreraDesigns <denvermaas@gmail.com>
2020-06-23 23:56:06 -0600

    created the config folder with connection.js and orm.js

---
+++ config/connection.js
@@ -0,0 +1,20 @@
+const mysql = require("mysql");
+
+const connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "8Limb$Thai1978",
+    database: "burgers_db"
+});
+//connect to database
+connection.connect((err) => {
+    if (err) {
+        console.error("error connecting:" + err.stack);
+        return;
+    }
+    console.log("connecteed as id" + connection.threadid);
+});
+
+
+module.exports = connection
---
+++ config/orm.js
@@ -0,0 +1,14 @@
+const connection = require("./config/connection.js");
+
+//methods to use for app
+// selectAll()
+// insertOne()
+// updateOne()
+
+
+
+
+
+
+
+module.exports = orm;

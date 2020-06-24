bc9d2ab4c9619950cdcd74e944c5cb7e83fcc56f
NekoCarreraDesigns <denvermaas@gmail.com>
2020-06-23 23:07:31 -0600

    created the db folder with the schema.sql, and seed.sql files

---
+++ db/seeds.sql
@@ -0,0 +1,12 @@
+INSERT INTO burgers
+    (burger_name, devoured)
+VALUES
+    ("Double Bacon Cheese NO Fixins", false);
+INSERT INTO burgers
+    (burger_name, devoured)
+VALUES
+    ("Cheese Burger Extra cheese", false);
+INSERT INTO burgers
+    (burger_name, devoured)
+VALUES
+    ("Chili Cheeseburger", false);

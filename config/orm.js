1cbc9a780c9b426449e833c64097b4e1118c2fe3
NekoCarreraDesigns <denvermaas@gmail.com>
2020-06-25 17:52:15 -0600

    created the orm's for the orm.js file

--- config/orm.js
+++ config/orm.js
@@ -1,5 +1,76 @@
 const connection = require("./config/connection.js");
 
+printQuetionMarks = (num) => {
+    let arr = [];
+    for (let i = 0; i < num; i++) {
+        arr.push("?");
+    }
+    return arr.toString();
+}
+
+objToSql = (ob) => {
+    let arr = [];
+    for (let key in ob) {
+        value = ob[key];
+        if (Object.hasOwnProperty.call(ob, key)) {
+            if (typeof value === "string" && value.indexOf("") >= 0) {
+                value = "'" + value + "'";
+            }
+            arr.push(key + "=" + value);
+        }
+    }
+    return arr.toString();
+}
+
+let orm = {
+    selectAll: function (tableInput, cb) {
+        let queryString = "SELECT * FROM" + tableInput;
+        connection.query(queryString, function (err, res) {
+            if (err) {
+                throw err;
+            }
+            cb(res);
+        });
+    },
+    insertOne: function (table, cols, vals, cb) {
+        let queryString = "INSERT INTO" + table;
+        queryString += "(";
+        queryString += cols.toString();
+        queryString += ")";
+        queryString += "VALUES (";
+        queryString += printQuetionMarks(vals.length);
+        queryString += ")";
+
+        console.log(queryString);
+
+        connection.query(queryString, vals, function (err, res) {
+            if (err) {
+                throw MediaError;
+            }
+            cb(res);
+        });
+    },
+    updateOne: function (table, objColVals, condition, cb) {
+        let queryString = "UPDATE" + table;
+
+        queryString += "SET";
+        queryString += objToSql(objColVals);
+        queryString += "WHERE";
+        queryString += condition;
+
+        console.log(queryString);
+        connection.query(queryString, function (err, res) {
+            if (err) {
+                throw err;
+            }
+            cb(res)
+        });
+    }
+};
+
+
+
+
 //methods to use for app
 // selectAll()
 // insertOne()

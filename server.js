5c6fd68eef9abba7d43330f277eea9a030fe28aa
NekoCarreraDesigns <denvermaas@gmail.com>
2020-06-25 23:25:36 -0600

    created the server functions, and the handlebars, set up the middleware

--- server.js
+++ server.js
@@ -1,2 +1,26 @@
 const express = require("express");
+const router = require("./controllers/burgers_controllers.js");
+const exphbs = require("express-handlebars")
 
+const PORT = process.env.PORT || 8080;
+
+const app = express();
+
+app.use(express.static("public"));
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+app.use(router)
+
+app.listen(PORT, (err) => {
+    if (err) {
+        return res.status(500).end();
+    } else {
+        res.status(200);
+    }
+    console.log("Server listening on:" + PORT);
+})

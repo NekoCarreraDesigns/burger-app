babdbf6db3be391bf03ad1e335b0a8a829acd622
NekoCarreraDesigns <denvermaas@gmail.com>
2020-07-04 18:36:49 -0600

    removed one of the get routes

--- controllers/burgers_controllers.js
+++ controllers/burgers_controllers.js
@@ -2,10 +2,8 @@ const express = require("express");
 const burger = require("../models/burger.js");
 
 const router = express.Router();
+
 router.get("/", (req, res) => {
-    res.redirect("/burgers")
-})
-router.get("/burgers", (req, res) => {
     burger.all((data) => {
         console.log(data);
         res.render("index", { burgers: data });

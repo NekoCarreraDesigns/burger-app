f62419f6ec8b6b845e573ee8fa0eb36df0b7c3cf
NekoCarreraDesigns <denvermaas@gmail.com>
2020-07-09 20:26:33 -0600

    changed to process.env for password and user

--- config/connection.js
+++ config/connection.js
@@ -5,10 +5,21 @@ if (process.env.JAWSDB_URL) {
     connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
     connection = mysql.createConnection({
-        host: "localhost",
+        host: process.env.db_host,
         port: 3306,
-        user: "root",
-        password: "8Limb$Thai1978",
+        user: process.env.db_user,
+        password: process.env.db_pass,
         database: "burgers_db"
     });
 };
+//connect to database
+connection.connect((err) => {
+    if (err) {
+        console.error("error connecting:" + err.stack);
+        return;
+    }
+    console.log("connected as id:" + connection.threadId);
+});
+
+
+module.exports = connection;

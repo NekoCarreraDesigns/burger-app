dc6afd5c2d0cf956af82090712014c1548c5bef6
NekoCarreraDesigns <denvermaas@gmail.com>
2020-07-09 18:09:52 -0600

    took out unnecessary variable in connection.js

--- config/connection.js
+++ config/connection.js
@@ -1,14 +1,14 @@
 const mysql = require("mysql");
-const dotenv = require("dotenv").config()
-const connection
+//const dotenv = require("dotenv").config()
+
 if (process.env.JAWSDB_URL) {
     connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
     connection = mysql.createConnection({
-        host: localhost,
+        host: "localhost",
         port: 3306,
-        user: process.env.db_user,
-        password: process.env.db_pass,
+        user: "root",
+        password: "8Limb$Thai1978",
         database: "burgers_db"
     });
 };

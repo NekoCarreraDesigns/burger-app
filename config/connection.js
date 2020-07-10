175139a298164b3bb8e1c610c8eb867020549d76
NekoCarreraDesigns <denvermaas@gmail.com>
2020-07-09 18:25:31 -0600

    removed unnecessary variable

--- config/connection.js
+++ config/connection.js
@@ -12,14 +12,3 @@ if (process.env.JAWSDB_URL) {
         database: "burgers_db"
     });
 };
-//connect to database
-connection.connect((err) => {
-    if (err) {
-        console.error("error connecting:" + err.stack);
-        return;
-    }
-    console.log("connected as id:" + connection.threadId);
-});
-
-
-module.exports = connection;
